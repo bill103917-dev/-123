97cc7f47e4b67f4061c35b4d51032214b9d01c2c
bill103917-dev <bill103917@gmail.com>
2025-08-30 06:41:47 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
- 
+export default {
+  async fetch(request, env) {
+    let url = new URL(request.url);
+    url.hostname = "disecord-bot2-u3z3.onrender.com"; // 只填域名
+
+    return fetch(url.toString(), {
+      method: request.method,
+      headers: request.headers,
+      body: request.body,
+      redirect: 'manual'
+    });
+  }
+}; 
